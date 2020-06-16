218109c851b029f9515165098c5d925410c87bda
YoungkonSong <yksong0303@gmail.com>
2020-06-16 10:56:48 +0900

    new class

--- src/s0616/Test10.java
+++ src/s0616/Test10.java
@@ -1,7 +1,28 @@
 package s0616;
 
+import java.util.Scanner;
+
 public class Test10 {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+	
+		System.out.print("어디살아?");
+		String address = scan.nextLine();
+		if(address.equals("서울")) { 
+//		if(address=="서울") { 이렇게 쓰면 성립안됨 ** 시험에 나옴
+			System.out.println("수도권");
+		}	else if(address.equals("남양주")) {
+			System.out.println("해외");
+			
+		}else {
+			System.out.println("지방");
+	}	
+		String str;
+//		String str1 = new String("123"); 원래는 이렇게 써야함
+		String str1 = "123";
+		str = "123";
+		System.out.println(str==str1);
+		
 		
 	}
 
---
+++ src/s0616/Test11.java
@@ -0,0 +1,13 @@
+package s0616;
+
+public class Test11 {
+	public static void main(String[] args) {
+		
+		
+		int[] nums = new int[4];
+		nums[0] = 3;
+		nums[1] = 1;
+		nums[2] = 5;
+		nums[3] = 7;
+	}
+}
